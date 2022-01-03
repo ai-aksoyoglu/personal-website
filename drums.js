3fe22a1e786516edf50f9f688302ebed288b68c8
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-01-03 21:17:23 +0100

    animation added to button click or key pressed for sound

--- drums.js
+++ drums.js
@@ -2,10 +2,12 @@ for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
   document.querySelectorAll('.drum')[i].addEventListener('click', function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 document.addEventListener('keydown', function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -34,4 +36,12 @@ function makeSound(key) {
     default:
       console.log(this.innerHTML);
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector('.' + currentKey);
+  activeButton.classList.add('pressed');
+  setTimeout(function () {
+    activeButton.classList.remove('pressed');
+  }, 100);
 }
