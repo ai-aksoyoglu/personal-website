411926d9a0942fdc068e5d4c11c6ec92f9f0eb72
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-01-03 20:18:15 +0100

    drums with sounds and switch case for each button

--- drums.js
+++ drums.js
@@ -1,5 +1,33 @@
-document.querySelector('button').addEventListener('click', handleclick);
+for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', handleclick);
+}
 
 function handleclick() {
-  alert('I was clicked');
+  var buttonInnerHTML = this.innerHTML;
+
+  switch (buttonInnerHTML) {
+    case 'w':
+      new Audio('assets/drums/tom-1.mp3').play();
+      break;
+    case 'a':
+      new Audio('assets/drums/tom-2.mp3').play();
+      break;
+    case 's':
+      new Audio('assets/drums/tom-3.mp3').play();
+      break;
+    case 'd':
+      new Audio('assets/drums/tom-4.mp3').play();
+      break;
+    case 'j':
+      new Audio('assets/drums/snare.mp3').play();
+      break;
+    case 'k':
+      new Audio('assets/drums/crash.mp3').play();
+      break;
+    case 'l':
+      new Audio('assets/drums/kick-bass.mp3').play();
+      break;
+    default:
+      console.log(this.innerHTML);
+  }
 }
