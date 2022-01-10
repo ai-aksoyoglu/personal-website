3d30563045a2ea88d946cee309387f368532fe00
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-01-10 14:30:44 +0100

    finished section 15, i.e. Challenge 2: The Simon Game

--- simon.js
+++ simon.js
@@ -1,33 +1,57 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
+var buttonColours = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
 var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$("document").keypress(function (level) {
-  $("h1").html("Level " + level);
+$(document).keypress(function () {
   if (!started) {
-    $("h1").html("Level " + level);
+    $('#level-title').text('Level ' + level);
     nextSequence();
     started = true;
   }
 });
 
-$(".btn").click(function () {
-  var userChosenColour = $(this).attr("id");
+$('.btn').click(function () {
+  var userChosenColour = $(this).attr('id');
   userClickedPattern.push(userChosenColour);
   console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log('success');
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log('wrong');
+    playSound('wrong');
+
+    $('body').addClass('game-over');
+    setTimeout(function () {
+      $('body').removeClass('game-over');
+    }, 200);
+
+    $('#level-title').text('Game Over, Press Any Key to Restart');
+
+    startOver();
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
   level++;
-  $("h1").html("Level " + level);
+  $('#level-title').text('Level ' + level);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  $("#" + randomChosenColour)
+  $('#' + randomChosenColour)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
@@ -35,13 +59,19 @@ function nextSequence() {
 }
 
 function playSound(name) {
-  var audio = new Audio("assets/simon/" + name + ".mp3");
+  var audio = new Audio('assets/simon/' + name + '.mp3');
   audio.play();
 }
 
 function animatePress(currentColour) {
-  $("#" + currentColour).addClass("pressed");
+  $('#' + currentColour).addClass('pressed');
   setTimeout(function () {
-    $("#" + currentColour).removeClass("pressed");
+    $('#' + currentColour).removeClass('pressed');
   }, 100);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
