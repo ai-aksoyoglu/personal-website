c86515539b42a52a422a0ef3bffe3711dcea9d11
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-01-10 16:28:18 +0100

    app.get for different routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello');
 });
 
+app.get('/contact', function (req, res) {
+  res.send('Contact me at this email:_____');
+});
+
 app.listen(3000, function () {
   console.log('Server started on port 3000');
 });
