c84c254e6d3d5dba4fdd015c292eff5988aeab13
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-01-18 18:11:23 +0100

    the website has bmi and weather report buttons now

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send(
+    'Hello. Would you like to know the weather forcast? Check out http://localhost:3000/weather<br>Would you like to calculate your BMI? Check out https://localhost:3000/bmi'
+  );
+});
+
+/*__dirname = /home/alexandra/Desktop/_Web Development_/personal-website
+ */
+
+app.get('/bmi', function (req, res) {
+  res.sendFile(__dirname + '/bmi.html');
+});
+
+app.post('/bmi', function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var result = weight / (height * height);
+  res.send('<h1>The corresponding BMI is ' + result.toFixed(2) + '.</h1>');
+});
+
+app.get('/weather', function (req, res) {
+  res.sendFile(__dirname + '/weather.html');
+});
+
+app.post('/weather', function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = 'bbfc773fa75427fe40a14807a7863b2a';
+  const units = 'metric';
+  const url =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    query +
+    '&appid=' +
+    apiKey +
+    '&units=' +
+    units;
+  https
+    .get(url, (response) => {
+      console.log('statusCode:', response.statusCode);
+      console.log('headers:', response.headers);
+
+      response.on('data', function (data) {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+
+        res.write(
+          '<p>The weather is currently ' +
+            weatherDescription +
+            '. </p><img src=' +
+            imageURL +
+            '>'
+        );
+        res.write(
+          '<h1>The temperature in ' +
+            req.body.cityName +
+            ' is now ' +
+            temp +
+            ' degrees Celcius.</h1>'
+        );
+        res.send();
+      });
+    })
+    .on('error', (e) => {
+      console.error(e);
+    });
+});
+
+app.listen(3000, function () {
+  console.log('Server started running on port 3000');
+});
