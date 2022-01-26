e14bf406d7436f9b4a5fbfde0a6b84080d273deb
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-01-26 17:28:28 +0100

    console.log the title and post body

--- app.js
+++ app.js
@@ -220,5 +220,6 @@ app.get('/blog-compose', function (req, res) {
 });
 
 app.post('/blog-compose', function (req, res) {
-  console.log(req.body.postTitle);
+  const post = { title: req.body.postTitle, content: req.body.postBody };
+  console.log(post);
 });
