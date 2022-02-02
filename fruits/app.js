4db79893faadab444896200ecbc49d9d606a140a
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-02-02 16:38:02 +0100

    use schema to add a fruit

--- fruits/app.js
+++ fruits/app.js
@@ -3,34 +3,16 @@ const mongoose = require('mongoose');
 // Connection URL
 mongoose.connect('mongodb://localhost:27017/fruitsDB');
 
-async function run() {
-  try {
-    const db = client.db(dbName);
+// Defining the schema
+const { Schema } = mongoose;
 
-    //Get the documents collection with
-    const collection = db.collection('fruits');
+const fruitSchema = new Schema({
+  name: String, // String is shorthand for {type: String}
+  rating: Number,
+  review: String,
+});
 
-    // create an array of documents to insert
-    const docs = [
-      { name: 'Apple', score: 8, review: 'This fruit is great!' },
-      { name: 'Orange', score: 6, review: 'Maybe sour.' },
-      { name: 'Banana', score: 9, review: 'Very healthy' },
-    ];
+const Fruit = mongoose.model('Fruit', fruitSchema);
 
-    const insertDocuments = await collection.insertMany(docs);
-    console.log(`${insertDocuments.insertedCount} documents were inserted`);
-
-    console.log(
-      `In the fruits collection, in the ${dbName} database, there are now:`
-    );
-    const findDocuments = await collection.find({}).forEach((fruits) => {
-      console.log(fruits.name);
-    });
-  } finally {
-    await client.close(function (err) {
-      assert.equal(null, err);
-      console.log('------------');
-    });
-  }
-}
-run().catch(console.dir);
+const fruit = new Fruit({ name: 'Apple', rating: 7, review: 'Very healthy' });
+fruit.save().then(() => console.log('A fruit was added.'));
