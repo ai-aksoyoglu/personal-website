9bbf46a603b235b43fb9c66474e3292e864c303e
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-02-02 16:28:34 +0100

    start using mongoose instead of native driver for mongoDB

--- fruits/app_native.js
+++ fruits/app_native.js
@@ -1,10 +1,23 @@
-const mongoose = require('mongoose');
+const assert = require('assert');
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
 
 // Connection URL
-mongoose.connect('mongodb://localhost:27017/fruitsDB');
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'fruitsDB';
 
 async function run() {
   try {
+    // Use connect method to connect to the server
+    await client.connect(function (err) {
+      assert.equal(null, err);
+      console.log('Connected successfully to server');
+    });
+
     const db = client.db(dbName);
 
     //Get the documents collection with
