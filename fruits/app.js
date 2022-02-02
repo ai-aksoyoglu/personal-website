de716e87f7906f4e8d9f2789018263c4c59b23b9
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-02-02 14:58:59 +0100

    3 documents were successfully inserted, using mongodb insert multiple documents documentation

--- fruits/app.js
+++ fruits/app.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const { MongoClient } = require('mongodb');
 // or as an es module:
 // import { MongoClient } from 'mongodb'
@@ -9,40 +10,33 @@ const client = new MongoClient(url);
 // Database Name
 const dbName = 'fruitsDB';
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('fruits');
+async function run() {
+  try {
+    // Use connect method to connect to the server
+    await client.connect(function (err) {
+      assert.equal(null, err);
+      console.log('Connected successfully to server');
+    });
 
-  // the following code examples can be pasted here...
+    const db = client.db(dbName);
 
-  // create a document to insert
-  const doc = [
-    {
-      name: 'Apple',
-      score: 8,
-      review: 'This fruit is great! An apple a day keeps the doctor away.',
-    },
-    {
-      name: 'Orange',
-      score: 6,
-      review: 'Maybe sour.',
-    },
-    {
-      name: 'Banana',
-      score: 9,
-      review: 'Very healthy',
-    },
-  ];
-  const result = await collection.insertMany(doc);
-  console.log(`${result.insertedCount} documents were inserted`);
+    //Get the documents collection with
+    const collection = db.collection('fruits');
 
-  return 'done.';
+    // create an array of documents to insert
+    const docs = [
+      { name: 'Apple', score: 8, review: 'This fruit is great!' },
+      { name: 'Orange', score: 6, review: 'Maybe sour.' },
+      { name: 'Banana', score: 9, review: 'Very healthy' },
+    ];
+
+    const result = await collection.insertMany(docs);
+    console.log(`${result.insertedCount} documents were inserted`);
+  } finally {
+    await client.close(function (err) {
+      assert.equal(null, err);
+      console.log('Done.');
+    });
+  }
 }
-
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+run().catch(console.dir);
