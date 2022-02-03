9f3848c9820aa6176771978058d1818b3e75d3f1
Alexandra <alexandra.aksoyoglu@mymoria.de>
2022-02-03 18:20:58 +0100

    added connection.close by putting everything into a main async function that awaits for all promises

--- fruits/app.js
+++ fruits/app.js
@@ -1,53 +1,58 @@
 const mongoose = require('mongoose');
 
-// Connection URL
-mongoose.connect('mongodb://localhost:27017/fruitsDB');
+const main = async function () {
+  // Connection URL
+  await mongoose.connect('mongodb://localhost:27017/fruitsDB');
 
-const { Schema } = mongoose;
+  const { Schema } = mongoose;
 
-// Defining the schema for fruits
-const fruitSchema = new Schema({
-  name: String, // String is shorthand for {type: String}
-  rating: Number,
-  review: String,
-});
+  // Defining the schema for fruits
+  const fruitSchema = new Schema({
+    name: String, // String is shorthand for {type: String}
+    rating: Number,
+    review: String,
+  });
 
-const Fruit = mongoose.model('Fruit', fruitSchema);
+  const Fruit = mongoose.model('Fruit', fruitSchema);
 
-const apple = new Fruit({ name: 'Apple', rating: 7, review: 'Very healthy' });
-apple.save().then(() => console.log('A fruit was added.'));
+  const apple = new Fruit({ name: 'Apple', rating: 7, review: 'Very healthy' });
+  await apple.save().then(() => console.log('A fruit was added.'));
 
-// Defining a new schema for people
-const personSchema = new Schema({
-  name: String, // String is shorthand for {type: String}
-  age: Number,
-});
+  // Defining a new schema for people
+  const personSchema = new Schema({
+    name: String, // String is shorthand for {type: String}
+    age: Number,
+  });
 
-const Person = mongoose.model('Person', personSchema);
+  const Person = mongoose.model('Person', personSchema);
 
-const person = new Person({ name: 'John', rating: 37 });
-person.save().then(() => console.log('A person was added.'));
+  const person = new Person({ name: 'John', rating: 37 });
+  await person.save().then(() => console.log('A person was added.'));
 
-// Add more fruits
-const kiwi = new Fruit({ name: 'Kiwi', rating: 10, review: 'Might be sour' });
+  // Add more fruits
+  const kiwi = new Fruit({ name: 'Kiwi', rating: 10, review: 'Might be sour' });
 
-const orange = new Fruit({ name: 'Orange', rating: 4, review: 'A citric' });
+  const orange = new Fruit({ name: 'Orange', rating: 4, review: 'A citric' });
 
-const banana = new Fruit({ name: 'Banana', rating: 3, review: 'Delicious' });
-Fruit.insertMany([kiwi, orange, banana], function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Successfully added more fruits');
-  }
-});
+  const banana = new Fruit({ name: 'Banana', rating: 3, review: 'Delicious' });
+  await Fruit.insertMany([kiwi, orange, banana], function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Successfully added more fruits');
+    }
+  });
 
-// Read from the fruitsDB database
-Fruit.find(function (err, fruits) {
-  if (err) {
-    console.log(err);
-  } else {
-    //    console.log(fruits);
-    fruits.forEach((element) => console.log(element.name));
-  }
-});
+  // Read from the fruitsDB database
+  await Fruit.find(function (err, fruits) {
+    if (err) {
+      console.log(err);
+    } else {
+      //    console.log(fruits);
+      fruits.forEach((element) => console.log(element.name));
+    }
+  });
+  mongoose.connection.close();
+};
+
+main();
