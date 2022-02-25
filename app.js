7f61dda9810fecd78af70a13da7a070bdb621eb9
Alexandra <ai.aksoyoglu@protonmail.com>
2022-02-25 22:37:10 +0100

    dotenv for mailchimp and mongoDB credentials

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const https = require('https');
-const request = require('request');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const request = require("request");
+
+require("dotenv").config();
+//console.log(process.env); // remove this after you've confirmed it working
+
+const mongoose = require("mongoose");
 /* without mongodb
 const date = require(__dirname + '/date.js');*/
 
-const _ = require('lodash');
+const _ = require("lodash");
 const homeStartingContent =
-  'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
-  'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.';
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
-  'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const mailchimp = require('@mailchimp/mailchimp_marketing');
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 mailchimp.setConfig({
-  apiKey: '',
-  server: 'us20',
+  apiKey: process.env.mailchimp_apiKey,
+  server: "us20",
 });
 /*check if mailchimp works successfully (api call)
 async function run() {
@@ -28,9 +32,9 @@ run();*/
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 /* without mongodb
 const items = ['Buy food', 'Cook', 'Eat 🎉'];
@@ -39,80 +43,80 @@ let posts = [];
 let urlPostTitles = [];
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Server started running on port 3000');
+  console.log("Server started running on port 3000");
 });
 
-app.get('/', function (req, res) {
-  res.render('about');
+app.get("/", function (req, res) {
+  res.render("about");
 });
 
 /*__dirname = /home/alexandra/Desktop/_Web Development_/personal-website*/
 
-app.get('/bmi', function (req, res) {
-  res.sendFile(__dirname + '/bmi.html');
+app.get("/bmi", function (req, res) {
+  res.sendFile(__dirname + "/bmi.html");
 });
 
-app.post('/bmi', function (req, res) {
+app.post("/bmi", function (req, res) {
   var weight = parseFloat(req.body.weight);
   var height = parseFloat(req.body.height);
   var result = weight / (height * height);
-  res.send('<h1>The corresponding BMI is ' + result.toFixed(2) + '.</h1>');
+  res.send("<h1>The corresponding BMI is " + result.toFixed(2) + ".</h1>");
 });
 
-app.get('/weather', function (req, res) {
-  res.sendFile(__dirname + '/weather.html');
+app.get("/weather", function (req, res) {
+  res.sendFile(__dirname + "/weather.html");
 });
 
-app.post('/weather', function (req, res) {
+app.post("/weather", function (req, res) {
   const query = req.body.cityName;
-  const apiKey = 'bbfc773fa75427fe40a14807a7863b2a';
-  const units = 'metric';
+  const apiKey = "bbfc773fa75427fe40a14807a7863b2a";
+  const units = "metric";
   const url =
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
     query +
-    '&appid=' +
+    "&appid=" +
     apiKey +
-    '&units=' +
+    "&units=" +
     units;
   https
     .get(url, (response) => {
-      console.log('statusCode:', response.statusCode);
-      console.log('headers:', response.headers);
+      console.log("statusCode:", response.statusCode);
+      console.log("headers:", response.headers);
 
-      response.on('data', function (data) {
+      response.on("data", function (data) {
         const weatherData = JSON.parse(data);
         const temp = weatherData.main.temp;
         const weatherDescription = weatherData.weather[0].description;
         const icon = weatherData.weather[0].icon;
-        const imageURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
         res.write(
-          '<p>The weather is currently ' +
+          "<p>The weather is currently " +
             weatherDescription +
-            '. </p><img src=' +
+            ". </p><img src=" +
             imageURL +
-            '>'
+            ">"
         );
         res.write(
-          '<h1>The temperature in ' +
+          "<h1>The temperature in " +
             req.body.cityName +
-            ' is now ' +
+            " is now " +
             temp +
-            ' degrees Celcius.</h1>'
+            " degrees Celcius.</h1>"
         );
         res.send();
       });
     })
-    .on('error', (e) => {
+    .on("error", (e) => {
       console.error(e);
     });
 });
 
-app.get('/newsletter-signup', function (req, res) {
-  res.sendFile(__dirname + '/newsletter-signup.html');
+app.get("/newsletter-signup", function (req, res) {
+  res.sendFile(__dirname + "/newsletter-signup.html");
 });
 
-app.post('/newsletter-signup', function (req, res) {
+app.post("/newsletter-signup", function (req, res) {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
@@ -123,12 +127,12 @@ app.post('/newsletter-signup', function (req, res) {
     email: email,
   };
 
-  const listId = '';
+  const listId = process.env.mailchimp_listID;
 
   async function run() {
     const response = await mailchimp.lists.addListMember(listId, {
       email_address: subscribingUser.email,
-      status: 'subscribed',
+      status: "subscribed",
       merge_fields: {
         FNAME: subscribingUser.firstName,
         LNAME: subscribingUser.lastName,
@@ -136,29 +140,29 @@ app.post('/newsletter-signup', function (req, res) {
     });
 
     //If all goes well logging the contact's id
-    res.sendFile(__dirname + '/newsletter-success.html');
+    res.sendFile(__dirname + "/newsletter-success.html");
     console.log(
       "Successfully added contact as an audience member. The contact's id is" +
         response.id +
-        '.'
+        "."
     );
   }
 
   //Running the function and catching the errors (if any)
   // ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LECTURE*************************
   // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
-  run().catch((e) => res.sendFile(__dirname + '/newsletter-failure.html'));
+  run().catch((e) => res.sendFile(__dirname + "/newsletter-failure.html"));
 });
 
-app.post('/newsletter-failure', function (req, res) {
-  res.redirect('/newsletter-signup');
+app.post("/newsletter-failure", function (req, res) {
+  res.redirect("/newsletter-signup");
 });
 
-/*const url = "https://us20.api.mailchimp.com/3.0/lists/" + list_id;
+/*const url = "https://us20.api.mailchimp.com/3.0/lists/" + process.env.mailchimp_listID;
 
   const options = {
     method: "POST",
-    auth: "ai.aksoyoglu:",
+    auth: "ai.aksoyoglu:process.env.mailchimp_apiKey",
   };
 
   const request = https.request(url, options, function (response) {
@@ -181,10 +185,17 @@ app.post('/newsletter-failure', function (req, res) {
       email +
       ". Thank you for signing up for the newsletter service.</h1>"
   );
-});*/
+});
 
 // With mongodb
-mongoose.connect('mongodb://localhost:27017/todolistDB');
+mongoose.connect('mongodb://localhost:27017/todolistDB');*/
+
+// With MongoDB Cloud Atlas
+const mongoDB_username = process.env.mongoDB_username;
+const mongoDB_password = process.env.mongoDB_password;
+mongoose.connect(
+  `mongodb+srv://${mongoDB_username}:${mongoDB_password}@cluster0.jrfbq.mongodb.net/todolistDB`
+);
 
 const { Schema } = mongoose;
 
@@ -192,19 +203,19 @@ const { Schema } = mongoose;
 const itemsSchema = new Schema({
   name: {
     type: String,
-    required: [true, 'No name was specified for this item'],
+    required: [true, "No name was specified for this item"],
   },
 });
 
-const Item = mongoose.model('Item', itemsSchema);
+const Item = mongoose.model("Item", itemsSchema);
 const item1 = new Item({
-  name: 'Buy Food',
+  name: "Buy Food",
 });
 const item2 = new Item({
-  name: 'Cook',
+  name: "Cook",
 });
 const item3 = new Item({
-  name: 'Eat 🎉',
+  name: "Eat 🎉",
 });
 const defaultItems = [item1, item2, item3];
 
@@ -213,9 +224,9 @@ const listSchema = new mongoose.Schema({
   items: [itemsSchema],
 });
 
-const List = mongoose.model('List', listSchema);
+const List = mongoose.model("List", listSchema);
 
-app.get('/todolist', function (req, res) {
+app.get("/todolist", function (req, res) {
   /* without mongodb
   const day = date.getDay();
   const day = date.getDate();*/
@@ -230,14 +241,14 @@ app.get('/todolist', function (req, res) {
           if (err) {
             console.log(err);
           } else {
-            console.log('Successfully added the 3 default items');
+            console.log("Successfully added the 3 default items");
           }
         });
-        res.redirect('/todolist');
+        res.redirect("/todolist");
       } else {
-        res.render('list', {
-          listTitle: 'Today',
-          listValue: 'today',
+        res.render("list", {
+          listTitle: "Today",
+          listValue: "today",
           newListItems: foundItems,
         });
       }
@@ -245,33 +256,33 @@ app.get('/todolist', function (req, res) {
   });
 });
 
-app.post('/todolist', function (req, res) {
+app.post("/todolist", function (req, res) {
   const itemName = req.body.newItem;
 
   const item = new Item({
     name: itemName,
   });
 
-  if (req.body.list === 'today') {
+  if (req.body.list === "today") {
     item.save();
-    res.redirect('/todolist');
+    res.redirect("/todolist");
   } else {
     List.findOne({ name: req.body.list }, function (err, foundList) {
       foundList.items.push(item);
       foundList.save();
-      res.redirect('/' + req.body.list);
+      res.redirect("/" + req.body.list);
     });
   }
 });
 
-app.post('/todolist/deleteItem', function (req, res) {
+app.post("/todolist/deleteItem", function (req, res) {
   const checkedItemId = req.body.checkbox;
 
-  if (req.body.listCheckedOff === 'today') {
+  if (req.body.listCheckedOff === "today") {
     Item.findByIdAndRemove(checkedItemId, function (err) {
       if (!err) {
-        console.log('Successfully deleted the checked item');
-        res.redirect('/todolist');
+        console.log("Successfully deleted the checked item");
+        res.redirect("/todolist");
       }
     });
   } else {
@@ -280,31 +291,31 @@ app.post('/todolist/deleteItem', function (req, res) {
       { $pull: { items: { _id: checkedItemId } } },
       function (err, foundList) {
         if (!err) {
-          res.redirect('/' + req.body.listCheckedOff);
+          res.redirect("/" + req.body.listCheckedOff);
         }
       }
     );
   }
 });
 
-app.get('/:customListName', function (req, res) {
+app.get("/:customListName", function (req, res) {
   var customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName }, async function (err, foundList) {
     if (!err) {
       if (!foundList) {
         console.log(
-          'The ' + customListName + ' does not exist yet, it will be created.'
+          "The " + customListName + " does not exist yet, it will be created."
         );
         const list = new List({
           name: customListName,
           items: defaultItems,
         });
         await list.save();
-        res.redirect('/' + customListName);
+        res.redirect("/" + customListName);
       } else {
-        console.log('The ' + foundList.name + ' list already exists!');
-        res.render('list', {
+        console.log("The " + foundList.name + " list already exists!");
+        res.render("list", {
           listTitle: foundList.name,
           listValue: foundList.name,
           newListItems: foundList.items,
@@ -314,38 +325,38 @@ app.get('/:customListName', function (req, res) {
   });
 });
 
-app.get('/blog-home', function (req, res) {
-  res.render('blog-home', { homeContent: homeStartingContent, posts: posts });
+app.get("/blog-home", function (req, res) {
+  res.render("blog-home", { homeContent: homeStartingContent, posts: posts });
 });
 
-app.get('/blog-about', function (req, res) {
-  res.render('blog-about', { aboutContent: aboutContent });
+app.get("/blog-about", function (req, res) {
+  res.render("blog-about", { aboutContent: aboutContent });
 });
 
-app.get('/blog-contact', function (req, res) {
-  res.render('blog-contact', { contactContent: contactContent });
+app.get("/blog-contact", function (req, res) {
+  res.render("blog-contact", { contactContent: contactContent });
 });
 
-app.get('/blog-compose', function (req, res) {
-  res.render('blog-compose', {});
+app.get("/blog-compose", function (req, res) {
+  res.render("blog-compose", {});
 });
 
-app.post('/blog-compose', function (req, res) {
+app.post("/blog-compose", function (req, res) {
   const post = { title: req.body.postTitle, content: req.body.postBody };
   posts.push(post);
   urlPostTitles.push(_.kebabCase(post.title));
-  res.redirect('/blog-home');
+  res.redirect("/blog-home");
 });
 
-app.get('/posts/:postName', function (req, res) {
+app.get("/posts/:postName", function (req, res) {
   var requestedTitle = _.kebabCase(req.params.postName);
   if (urlPostTitles.includes(requestedTitle)) {
     posts.forEach(function (post) {
       if (requestedTitle === _.kebabCase(post.title)) {
-        res.render('blog-post', { post: post });
+        res.render("blog-post", { post: post });
       }
     });
   } else {
-    res.redirect('/blog-home');
+    res.redirect("/blog-home");
   }
 });
